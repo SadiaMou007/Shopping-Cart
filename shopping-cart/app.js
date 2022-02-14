88b84887c3a0ad09d0e164a1d84211e85969cbc7
Your Name <you@example.com>
2022-02-14 03:00:03 -0800

    add total and tax claulation

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,14 +1,46 @@
-const textNumber = document.getElementById("phone-counter");
-const phonePlusIcon = document.getElementById("phone-plus-icon");
-const phoneMinusICon = document.getElementById("phone-minus-icon");
-
-phonePlusIcon.addEventListener("click", function () {
-  textNumber.value = parseInt(textNumber.value) + 1;
-});
-
-phoneMinusICon.addEventListener("click", function () {
-  if (textNumber.value == 0) {
-    return;
+function updateQuantity(quantity, individualCost, Price, isIncrease) {
+  const productQuantity = document.getElementById(quantity);
+  let quantityValue = parseInt(productQuantity.value);
+  if (isIncrease) {
+    quantityValue++;
+    productQuantity.value = quantityValue;
+  } else if (quantityValue > 0) {
+    quantityValue--;
+    productQuantity.value = quantityValue;
   }
-  textNumber.value = parseInt(textNumber.value) - 1;
+  const priceId = document.getElementById(Price);
+  const totalPrice = quantityValue * individualCost;
+  priceId.innerText = totalPrice;
+
+  calculateTotal();
+}
+//  cost of phone
+function subTotal(product) {
+  const item = document.getElementById(product + "-quantity");
+  const itemNumber = parseInt(item.value);
+  return itemNumber;
+}
+
+function calculateTotal() {
+  const phoneNumber = subTotal("phone");
+  const caseNumber = subTotal("case");
+  const total = phoneNumber * 1219 + caseNumber * 59;
+  const tax = total * 0.1;
+
+  document.getElementById("sub-total").innerText = total;
+  document.getElementById("tax").innerText = tax.toFixed(2);
+  document.getElementById("grand-total").innerText = tax + total;
+}
+
+document.getElementById("phone-plus").addEventListener("click", function () {
+  updateQuantity("phone-quantity", 1219, "phone-cost", true);
+});
+document.getElementById("phone-minus").addEventListener("click", function () {
+  updateQuantity("phone-quantity", 1219, "phone-cost", false);
+});
+document.getElementById("case-plus").addEventListener("click", function () {
+  updateQuantity("case-quantity", 59, "case-cost", true);
+});
+document.getElementById("case-minus").addEventListener("click", function () {
+  updateQuantity("case-quantity", 59, "case-cost", false);
 });
