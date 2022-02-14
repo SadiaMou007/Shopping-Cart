59acd0c612031996a7ebafd000258d24b4240774
Your Name <you@example.com>
2022-02-13 23:11:00 -0800

    add event to phone number

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,1 +1,14 @@
-console.log('I am in a separate file')
+const textNumber = document.getElementById("phone-counter");
+const phonePlusIcon = document.getElementById("phone-plus-icon");
+const phoneMinusICon = document.getElementById("phone-minus-icon");
+
+phonePlusIcon.addEventListener("click", function () {
+  textNumber.value = parseInt(textNumber.value) + 1;
+});
+
+phoneMinusICon.addEventListener("click", function () {
+  if (textNumber.value == 0) {
+    return;
+  }
+  textNumber.value = parseInt(textNumber.value) - 1;
+});
